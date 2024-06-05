449083a92635e0c6567b39e69f8ff61809691111
yagofeitoza19 <113868936+yagofeitoza19@users.noreply.github.com>
2024-06-05 15:32:36 -0400

    Create graph.c

---
+++ Grafo - Lista de Adjacência/04.06.2024/graph.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "graph.h"
+
+AdjListNode* newAdjListNode(int dest) {
+    AdjListNode* newNode = (AdjListNode*)malloc(sizeof(AdjListNode));
+    newNode->dest = dest;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Graph* createGraph(int V) {
+    Graph* graph = (Graph*)malloc(sizeof(Graph));
+    graph->V = V;
+
+    graph->array = (AdjList*)malloc(V * sizeof(AdjList));
+
+    for (int i = 0; i < V; ++i) {
+        graph->array[i].head = NULL;
+    }
+
+    return graph;
+}
+
+void addEdge(Graph* graph, int src, int dest) {
+    AdjListNode* newNode = newAdjListNode(dest);
+    newNode->next = graph->array[src].head;
+    graph->array[src].head = newNode;
+
+    newNode = newAdjListNode(src);
+    newNode->next = graph->array[dest].head;
+    graph->array[dest].head = newNode;
+}
+
+void printGraph(Graph* graph) {
+    for (int v = 0; v < graph->V; ++v) {
+        AdjListNode* pCrawl = graph->array[v].head;
+        printf("\n Lista de adjacência do vértice %d\n head", v);
+        while (pCrawl) {
+            printf(" -> %d", pCrawl->dest);
+            pCrawl = pCrawl->next;
+        }
+        printf("\n");
+    }
+}
+
+void freeGraph(Graph* graph) {
+    for (int v = 0; v < graph->V; ++v) {
+        AdjListNode* adjListPtr = graph->array[v].head;
+        while (adjListPtr) {
+            AdjListNode* temp = adjListPtr;
+            adjListPtr = adjListPtr->next;
+            free(temp);
+        }
+    }
+    free(graph->array);
+    free(graph);
+}
